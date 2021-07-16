2e962e056b85e5bff6aec3cc9c5f760d3435cbf5
cchavezlo <boniee.ch.l@gmail.com>
2021-07-16 11:07:07 -0500

    cchavezlo

--- functions.py
+++ functions.py
@@ -12,11 +12,8 @@ import pandas as pd
 import collections
 import statsmodels.api as sm
 
-scipy import stats as st
 from scipy import fft
 from scipy import signal as sig
-
-import statistics as stats
 
 from attrs import attr_data
 
@@ -56,11 +53,6 @@ def porcDeNullsXColumn(df):
 def compValNull(df):
     mod = SimpleImputer(missing_values="?", strategy="most_frequent")
     dfcompleto = pd.DataFrame(mod.fit_transform(df))
-    i=0
-    for c in dfcompleto.columns:
-        if(i>1):
-            dfcompleto[c]=np.array(dfcompleto[c],dtype=float)
-        i=i+1
     return (dfcompleto)
 
 
@@ -68,39 +60,15 @@ def maxYmin(df):
     dfcompleto = compValNull(df)
     maximo = dfcompleto.max()
     minimo = dfcompleto.min()
-    
+    print(minimo)
     return maximo, minimo
 
 
-def mediana(df):
-    dfcompleto = compValNull(df)
-    mediana = []
-    for c in dfcompleto.columns:
-        if(isinstance(dfcompleto[c][100],str)):
-            mediana.append(-1)
-        else:
-            mediana.append(stats.median(edades))
+def knnOp(df, ng):
+    imputer = KNNImputer(missing_values="?", n_neighbors=ng)
+    imputer.fit_transform(df)
+    return df
 
-    return mediana
-
-def media(df):
-    dfcompleto = compValNull(df)
-    media = []
-    for c in dfcompleto.columns:
-        if(isinstance(dfcompleto[c][100],str)):
-            media.append(-1)
-        else:
-            media.append(dfcompleto[c].mean())
-
-    return media
-
-def moda(df):
-    dfcompleto = compValNull(df)
-    moda = []
-    for c in dfcompleto.columns:
-        moda.append(st.mode(dfcompleto[c]))
-
-    return moda
 
 def get_columns():
     columns = []
@@ -124,6 +92,7 @@ def leerCSV(ruta, rutaNames=False):
         df = pd.read_csv(ruta)
         return df
 
+
 def open_dataset():
     dataframe = pd.read_csv('db.txt', sep=',', header=None)
     # columns = dataframe.iloc[0]
@@ -134,13 +103,21 @@ def open_dataset():
     return dataframe
 
 
-def get_lost_data(dataframe):
-    res = {}
+def get_lost_data(dataframe, return_only_nulls=False):
+    column_detail = {}
     for column in dataframe.columns:
-        lost_data, indices = dataframe.get_lost_data(column)
-
-        res[column] = {'porcentaje': lost_data, 'indices': indices}
-    return res
+        null_num = (dataframe[column] == '?').sum()
+        null_percentage = round(null_num * 100 /
+                                len(dataframe[column]), 2)
+        # print(f"{column}: ", null_num, null_percentage)
+        if return_only_nulls:
+            if null_percentage != 0:
+                column_detail[column] = null_percentage
+            else:
+                continue
+        else:
+            column_detail[column] = null_percentage
+    return column_detail
 
 
 if __name__ == '__main__':
--- views/data_faltante.tpl
+++ views/data_faltante.tpl
@@ -4,4 +4,53 @@
     Exploración de datos:
 </h4>
 <hr />
+<h5>Información por {{type_info}}:</h5>
+<div class="divider"></div>
 
+<table>
+    <thead>
+    <tr>
+          <th>Mapa Informativo</th>
+    </tr>
+    <tr>
+        <div id="tooltip-container"></div>
+        <div id="canvas-svg"></div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/topojson/1.1.0/topojson.min.js"></script>
+
+        <script type="text/javascript" src="/static/us_states.js"></script>
+
+    </tr>
+    <tr>
+        <th></th>
+        %for label in columns_detail:
+        <th>{{label}}</th>
+        %end
+    </tr>
+    </thead>
+
+    <tbody>
+
+        <tr>
+            <th>Porcentaje de Valores nulos</th>
+
+            %for label in columns_detail:
+            <th>{{columns_detail[label]['perc']}}% - {{columns_detail[label]['len']}}</th>
+            %end
+        </tr>
+        %for column_name, column_data in columns_data.items():
+        <tr >
+            <td>{{column_name}}</td>
+            %for attr_name in columns_detail:
+
+            <td>{{column_data[attr_name]}}</td>
+            %end
+
+        </tr>
+        %end
+
+
+    </tbody>
+
+</table>
